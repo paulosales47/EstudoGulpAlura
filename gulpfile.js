a9dbca44e5ae87c29d48f0a2c0c4b64083305934
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-25 21:44:19 -0300

    Instalando e configurando plugun do gulp para concatenação de arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 let gulp = require('gulp');
 let imagemin = require('gulp-imagemin');
 let clean = require('gulp-clean');
+let concat = require('gulp-concat');
 
-function cl(){
+function clearDist(){
     return gulp.src('dist/**.*', {read: false})
     .pipe(clean());
 }
 
 function copy(){
     return gulp.src('src/**/*')
-    .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'));
 }
 
 function buildImg(){
@@ -18,5 +19,20 @@ function buildImg(){
         .pipe(gulp.dest('dist/img'));
 }
 
-exports.default = gulp.series(cl, copy, buildImg);
+function buildJs(){
+    return gulp.src('dist/js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+}
+
+
+
+exports.default =
+     gulp.series(
+        clearDist, 
+        copy, 
+    gulp.parallel(
+        buildImg,
+        buildJs
+    ));
 
